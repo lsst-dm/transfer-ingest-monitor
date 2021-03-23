48b8fee60dc0a4529be1cb048aee000b3abfe0c2
T. Andrew Manning <manninga@illinois.edu>
2021-03-23 16:34:40 -0500

    Added CronJob to run cron_observing_script.sh every 5 min

--- src/transfer-ingest-monitor/src/cron_observing_monitor.sh
+++ src/transfer-ingest-monitor/src/cron_observing_monitor.sh
@@ -1,13 +1,13 @@
-source /software/lsstsw/stack3/loadLSST.bash
-WEBDIR=/lsstdata/offline/web_data/processing_monitor
+# source /software/lsstsw/stack3/loadLSST.bash
+WEBDIR=$HOME/public_html/transfer-ingest-monitor
 CRONPATH=/home/lsstdbot/SOFTWARE/observing_monitor
 REPO=/lsstdata/offline/teststand
-$CRONPATH/break_locks.py $WEBDIR/auxTel $WEBDIR/NCSA_auxTel $WEBDIR/BOT $WEBDIR/comcam_ccs $WEBDIR/NCSA_comcam $WEBDIR/comcam_archiver
-$CRONPATH/observing_monitor.py --input_dir $REPO/auxTel/L1Archiver --output $WEBDIR/auxTel
-$CRONPATH/observing_monitor.py --input_dir $REPO/auxTel/L1Archiver --output $WEBDIR/auxTel --gen 3
-$CRONPATH/observing_monitor.py --input_dir $REPO/NCSA_auxTel --output $WEBDIR/NCSA_auxTel
-$CRONPATH/observing_monitor.py --input_dir $REPO/BOT --output $WEBDIR/BOT --num_days 3
-$CRONPATH/observing_monitor.py --input_dir $REPO/comcam/CCS/ --output $WEBDIR/comcam_ccs
-$CRONPATH/observing_monitor.py --input_dir $REPO/NCSA_comcam/ --output $WEBDIR/NCSA_comcam
-$CRONPATH/observing_monitor.py --input_dir $REPO/comcam/Archiver --output $WEBDIR/comcam_archiver
-$CRONPATH/mainpage.py $WEBDIR auxTel BOT comcam_archiver comcam_ccs NCSA_auxTel  NCSA_comcam
+python3 break_locks.py $WEBDIR/auxTel $WEBDIR/NCSA_auxTel $WEBDIR/BOT $WEBDIR/comcam_ccs $WEBDIR/NCSA_comcam $WEBDIR/comcam_archiver
+python3 observing_monitor.py --input_dir $REPO/auxTel/L1Archiver --output $WEBDIR/auxTel
+python3 observing_monitor.py --input_dir $REPO/auxTel/L1Archiver --output $WEBDIR/auxTel --gen 3
+python3 observing_monitor.py --input_dir $REPO/NCSA_auxTel --output $WEBDIR/NCSA_auxTel
+python3 observing_monitor.py --input_dir $REPO/BOT --output $WEBDIR/BOT --num_days 3
+python3 observing_monitor.py --input_dir $REPO/comcam/CCS/ --output $WEBDIR/comcam_ccs
+python3 observing_monitor.py --input_dir $REPO/NCSA_comcam/ --output $WEBDIR/NCSA_comcam
+python3 observing_monitor.py --input_dir $REPO/comcam/Archiver --output $WEBDIR/comcam_archiver
+python3 mainpage.py $WEBDIR auxTel BOT comcam_archiver comcam_ccs NCSA_auxTel  NCSA_comcam
