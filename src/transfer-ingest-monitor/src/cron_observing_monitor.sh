9dcb45d50f208c63f9afc04abc2cd896faa4d95c
T. Andrew Manning <manninga@illinois.edu>
2021-03-26 16:49:11 -0500

    Parameterize number of days to monitor in Helm chart values.

--- src/transfer-ingest-monitor/src/cron_observing_monitor.sh
+++ src/transfer-ingest-monitor/src/cron_observing_monitor.sh
@@ -8,13 +8,13 @@ REPO_NTS=/lsstdata/offline/teststand
 # python3 break_locks.py $WEBDIR/auxTel $WEBDIR/NCSA_auxTel $WEBDIR/LSSTCam-bot $WEBDIR/comcam_ccs $WEBDIR/NCSA_comcam $WEBDIR/comcam_archiver
 
 # Gather transfer information for each data source being monitored
-python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_auxTel --output $WEBDIR/NCSA_auxTel --num_days 3
-python3 observing_monitor.py --input_dir $REPO/LATISS --output $WEBDIR/auxTel --num_days 3
-python3 observing_monitor.py --input_dir $REPO/LATISS --output $WEBDIR/auxTel --num_days 3 --gen 3 
-python3 observing_monitor.py --input_dir $REPO/LSSTCam-bot --output $WEBDIR/LSSTCam-bot --num_days 3
-python3 observing_monitor.py --input_dir $REPO/LSSTComCam-ccs/ --output $WEBDIR/comcam_ccs --num_days 3
-python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_comcam/ --output $WEBDIR/NCSA_comcam --num_days 3
-python3 observing_monitor.py --input_dir $REPO/LSSTComCam --output $WEBDIR/comcam_archiver --num_days 3
+python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_auxTel --output $WEBDIR/NCSA_auxTel --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO/LATISS --output $WEBDIR/auxTel --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO/LATISS --output $WEBDIR/auxTel --num_days $MONITOR_NUM_DAYS --gen 3 
+python3 observing_monitor.py --input_dir $REPO/LSSTCam-bot --output $WEBDIR/LSSTCam-bot --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO/LSSTComCam-ccs/ --output $WEBDIR/comcam_ccs --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_comcam/ --output $WEBDIR/NCSA_comcam --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO/LSSTComCam --output $WEBDIR/comcam_archiver --num_days $MONITOR_NUM_DAYS
 # Render summary page
 python3 mainpage.py $WEBDIR \
     auxTel \
