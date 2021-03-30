1a35d495221c03fa5e37258408987f594889e016
T. Andrew Manning <manninga@illinois.edu>
2021-03-29 20:51:55 -0500

    Success querying EFD for ATCamera exposure info

--- src/transfer-ingest-monitor/src/efd_query.py
+++ src/transfer-ingest-monitor/src/efd_query.py
@@ -1,40 +1,72 @@
 from astropy.time import Time, TimeDelta
 from lsst_efd_client import EfdClient
 import asyncio
+from datetime import datetime
+import pandas as pd
 
-client = EfdClient('ldf_int_efd')
-cl = efd.influx_client
+client = EfdClient('ldf_stable_efd')
+cl = client.influx_client
 
-t2_agg = Time('2021-03-15T12:00:00', scale='tai')
-t1_agg = t2_agg - TimeDelta(5*24*3600, format='sec', scale='tai') # look back over 5 days of data
+t2_agg = Time('2021-03-24T12:00:00', scale='tai')
+t1_agg = t2_agg - TimeDelta(15*24*3600, format='sec', scale='tai')
 
 t2_chunk = Time('2021-03-04T12:00:00', scale='tai')
 t1_chunk = Time('2021-02-04T12:00:00', scale='tai')
 
 def make_query_str(beg, end, interval):
-    return ("SELECT mean(\"ambient_temp\") as \"temp\", mean(\"pressure\") as \"pressure\", mean(\"humidity\") as \"humidity\" " +
-            "FROM \"efd\".\"autogen\".\"lsst.sal.WeatherStation.weather\" " +
-            f"WHERE time > '{beg.isot}Z' and time <= '{end.isot}Z' " +
-            f"GROUP BY time({interval})")
-            
+    # query = f'''
+    #         SELECT "expTime"
+    #         FROM "efd"."autogen"."lsst.sal.ATCamera.command_takeImages" 
+    #         WHERE time > '{beg.isot}Z' and time <= '{end.isot}Z' 
+    #         '''
+    query = f'''
+            SELECT 
+                "imageName",
+                "imageDate",
+                "imageIndex",
+                "private_sndStamp",
+                "private_rcvStamp",
+                "timestampAcquisitionStart",
+                "timestampEndOfReadout",
+                "timeStampEndOfReadout",
+                "timeStampAcquisitionStart",
+                "imageSource" 
+            FROM "efd"."autogen"."lsst.sal.ATCamera.logevent_endReadout" 
+            WHERE time > '{beg.isot}Z' and time <= '{end.isot}Z' 
+            '''
+    print(query)
+    return query
+    
 async def get_topics():
     topics = await client.get_topics()
     print(topics)
     return topics
     
 async def query_topic(topic='lsst.sal.ATCamera.wreb'):
-    t2 = Time('2021-03-01T12:00:00', scale='tai')
+    t2 = Time('2020-03-16T12:00:00', scale='tai')
     t1 = t2 - TimeDelta(15*24*3600, format='sec', scale='tai')
+    print('Querying EFD...')
     df = await client.select_time_series(topic, '*', t1, t2)
     print(df.head())
     
 async def submit_query():
     result = await cl.query(make_query_str(t1_agg, t2_agg, '30m'))
+    print(result)
+    for index, row in result.iterrows():
+        print(f'''
+        imageName: {row['imageName']}
+        imageDate: {row['imageDate']}
+        timestampAcquisitionStart: {row['timestampAcquisitionStart']} --> {datetime.fromtimestamp(row['timestampAcquisitionStart']).strftime("%m-%d-%YT%H:%M:%S.%f %z")}
+        timestampEndOfReadout: {row['timestampEndOfReadout']} --> {datetime.fromtimestamp(row['timestampEndOfReadout']).strftime("%m-%d-%YT%H:%M:%S.%f %z")}
+        ''')
+    return result
+    
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     tasks = [
         # loop.create_task(get_topics()),
-        loop.create_task(query_topic(topic='lsst.sal.ATCamera.command_takeImages')),
+        # loop.create_task(query_topic(topic='lsst.sal.ATCamera.wreb')),
+        loop.create_task(submit_query()),
     ]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
