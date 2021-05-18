578f8506d6bb95aa962326b5a970888c2499e568
T. Andrew Manning <manninga@illinois.edu>
2021-05-18 16:29:14 -0500

    Include the gen2 repos

--- src/transfer-ingest-monitor/src/cron_observing_monitor.sh
+++ src/transfer-ingest-monitor/src/cron_observing_monitor.sh
@@ -16,6 +16,13 @@ python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_comcam/ --output $WEBDIR
 python3 observing_monitor.py --input_dir $REPO/LSSTComCam --output $WEBDIR/comcam_archiver --num_days $MONITOR_NUM_DAYS --gen 3
 python3 observing_monitor.py --input_dir $REPO/LSSTCam-bot --output $WEBDIR/LSSTCam-bot --num_days $MONITOR_NUM_DAYS --gen 3
 
+python3 observing_monitor.py --input_dir $REPO/LATISS --output $WEBDIR/auxTel --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_auxTel --output $WEBDIR/NCSA_auxTel --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO/LSSTComCam-ccs/ --output $WEBDIR/comcam_ccs --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_comcam/ --output $WEBDIR/NCSA_comcam --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO/LSSTComCam --output $WEBDIR/comcam_archiver --num_days $MONITOR_NUM_DAYS
+python3 observing_monitor.py --input_dir $REPO/LSSTCam-bot --output $WEBDIR/LSSTCam-bot --num_days $MONITOR_NUM_DAYS
+
 # Render the webpage
 #
 python3 mainpage.py $WEBDIR \
