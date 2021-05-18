a0bcc59e4790dc77c0aec571aeb1b6cbc5058e2f
T. Andrew Manning <manninga@illinois.edu>
2021-05-18 13:49:49 -0500

    Add multi-release support to sync script

--- src/transfer-ingest-monitor/k8s_sync_scripts.sh
+++ src/transfer-ingest-monitor/k8s_sync_scripts.sh
@@ -11,7 +11,11 @@ if [[ "X${NAMESPACE}" == "X" ]]; then
   NAMESPACE="lsst-dm"
 fi
 
-DEPLOYMENT_NAME="transfer-ingest-monitor"
+DEPLOYMENT_NAME="$2"
+if [[ "X${DEPLOYMENT_NAME}" == "X" ]]; then
+  DEPLOYMENT_NAME="transfer-ingest-monitor"
+fi
+
 SOURCE_DIR="src"
 TARGET_DIR="/home/worker/app"
 
@@ -24,10 +28,10 @@ STRIP_NUM="$(echo "${SOURCE_DIR}" | tr '/' ' ' | wc -w)"
 # kubectl exec -i -n "${NAMESPACE}" --container="${DEPLOYMENT_NAME}" "${POD_ID}" -- id
 # kubectl exec -i -n "${NAMESPACE}" --container="${DEPLOYMENT_NAME}" "${POD_ID}" -- ls -l "${TARGET_DIR}"
 tar cf - "${SOURCE_DIR}" | \
-    kubectl exec -i -n "${NAMESPACE}" --container="${DEPLOYMENT_NAME}" "${POD_ID}" -- \
+    kubectl exec -i -n "${NAMESPACE}" "${POD_ID}" -- \
     tar --overwrite -xf - --strip-components="${STRIP_NUM}" -C "${TARGET_DIR}"
 
-LASTSYNC="$(kubectl exec -it -n "${NAMESPACE}" --container="${DEPLOYMENT_NAME}" "${POD_ID}" -- cat "${TARGET_DIR}/.codesync" | tr -d '\n' | tr -d '\r')"
+LASTSYNC="$(kubectl exec -it -n "${NAMESPACE}" "${POD_ID}" -- cat "${TARGET_DIR}/.codesync" | tr -d '\n' | tr -d '\r')"
 # LASTSYNC="$(echo "${LASTSYNC}" | tr -d '\n' | tr -d '\r')"
 rm -f "${SOURCE_DIR}/.codesync"
 if [[ "${LASTSYNC}" == "${TIMESTAMP}" ]]; then
