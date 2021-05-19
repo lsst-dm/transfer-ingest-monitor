0bc729041d524afeb339fc8d2cd016c6c560b958
T. Andrew Manning <manninga@illinois.edu>
2021-05-19 16:11:15 -0500

    Use data source configmap to parameterize data source config

--- src/transfer-ingest-monitor/src/cron_observing_monitor.sh
+++ src/transfer-ingest-monitor/src/cron_observing_monitor.sh
@@ -9,26 +9,23 @@ REPO_NTS=/lsstdata/offline/teststand
 
 # Gather transfer information for each data source being monitored
 #
-python3 observing_monitor.py --input_dir $REPO/LATISS --output $WEBDIR/auxTel --num_days $MONITOR_NUM_DAYS --gen 3 
-python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_auxTel --output $WEBDIR/NCSA_auxTel --num_days $MONITOR_NUM_DAYS --gen 3 
-python3 observing_monitor.py --input_dir $REPO/LSSTComCam-ccs/ --output $WEBDIR/comcam_ccs --num_days $MONITOR_NUM_DAYS --gen 3
-python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_comcam/ --output $WEBDIR/NCSA_comcam --num_days $MONITOR_NUM_DAYS --gen 3
-python3 observing_monitor.py --input_dir $REPO/LSSTComCam --output $WEBDIR/comcam_archiver --num_days $MONITOR_NUM_DAYS --gen 3
-python3 observing_monitor.py --input_dir $REPO/LSSTCam-bot --output $WEBDIR/LSSTCam-bot --num_days $MONITOR_NUM_DAYS --gen 3
-
-python3 observing_monitor.py --input_dir $REPO/LATISS --output $WEBDIR/auxTel --num_days $MONITOR_NUM_DAYS
-python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_auxTel --output $WEBDIR/NCSA_auxTel --num_days $MONITOR_NUM_DAYS
-python3 observing_monitor.py --input_dir $REPO/LSSTComCam-ccs/ --output $WEBDIR/comcam_ccs --num_days $MONITOR_NUM_DAYS
-python3 observing_monitor.py --input_dir $REPO_NTS/NCSA_comcam/ --output $WEBDIR/NCSA_comcam --num_days $MONITOR_NUM_DAYS
-python3 observing_monitor.py --input_dir $REPO/LSSTComCam --output $WEBDIR/comcam_archiver --num_days $MONITOR_NUM_DAYS
-python3 observing_monitor.py --input_dir $REPO/LSSTCam-bot --output $WEBDIR/LSSTCam-bot --num_days $MONITOR_NUM_DAYS
+for gen in "--gen 3" "--gen 2"; do
+    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name auxtel_ccs --output $WEBDIR/auxtel --num_days $MONITOR_NUM_DAYS $gen 
+    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name auxtel_arc --output $WEBDIR/auxtel_archiver --num_days $MONITOR_NUM_DAYS $gen
+    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name nts_auxtel --output $WEBDIR/nts_auxtel --num_days $MONITOR_NUM_DAYS $gen 
+    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name comcam_ccs --output $WEBDIR/comcam --num_days $MONITOR_NUM_DAYS $gen
+    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name comcam_arc --output $WEBDIR/comcam_archiver --num_days $MONITOR_NUM_DAYS $gen
+    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name nts_comcam --output $WEBDIR/nts_comcam --num_days $MONITOR_NUM_DAYS $gen
+    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name bot --output $WEBDIR/bot --num_days $MONITOR_NUM_DAYS $gen
+done
 
 # Render the webpage
 #
 python3 mainpage.py $WEBDIR \
-    auxTel \
-    NCSA_auxTel \
-    comcam_ccs \
-    NCSA_comcam \
+    auxtel \
+    auxtel_archiver \
+    nts_auxtel \
+    comcam \
     comcam_archiver \
-    BOT
+    nts_comcam \
+    bot
