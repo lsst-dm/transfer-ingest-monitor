f99540a5aad2df04c42f34bf2f9dd66125419d86
T. Andrew Manning <manninga@illinois.edu>
2021-05-27 13:49:35 -0500

    Refactor CronJob script

--- src/transfer-ingest-monitor/src/cron_observing_monitor.sh
+++ src/transfer-ingest-monitor/src/cron_observing_monitor.sh
@@ -1,25 +1,20 @@
-# Output website files:
-WEBDIR=$HOME/public_html/$WEB_BASE_PATH
-
 # Gather transfer information for each data source being monitored
 #
-for gen in "--gen 3" "--gen 2"; do
-    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name auxtel_ccs --output $WEBDIR/auxtel_ccs --num_days $MONITOR_NUM_DAYS $gen # --first_day=20210501 --last_day=20210515
-    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name auxtel_arc --output $WEBDIR/auxtel_arc --num_days $MONITOR_NUM_DAYS $gen # --first_day=20210501 --last_day=20210515
-    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name comcam_ccs --output $WEBDIR/comcam_ccs --num_days $MONITOR_NUM_DAYS $gen # --first_day=20210501 --last_day=20210515
-    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name comcam_arc --output $WEBDIR/comcam_arc --num_days $MONITOR_NUM_DAYS $gen # --first_day=20210501 --last_day=20210515
-    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name nts_auxtel --output $WEBDIR/nts_auxtel --num_days $MONITOR_NUM_DAYS $gen # --first_day=20210501 --last_day=20210515
-    python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name nts_comcam --output $WEBDIR/nts_comcam --num_days $MONITOR_NUM_DAYS $gen # --first_day=20210501 --last_day=20210515
-    # python3 observing_monitor.py --source_config /etc/config/data_sources.yaml --source_name bot --output $WEBDIR/bot --num_days $MONITOR_NUM_DAYS $gen
+
+# data_sources=("auxtel_ccs" "auxtel_arc" "comcam_ccs" "comcam_arc" "nts_auxtel" "nts_comcam" "bot")
+data_sources=("auxtel_ccs" "auxtel_arc" "comcam_ccs" "comcam_arc" "nts_auxtel" "nts_comcam")
+for gen in "2" "3"; do
+    for data_source in "${data_sources[@]}"; do
+        python3 observing_monitor.py \
+            --source_config /etc/config/data_sources.yaml \
+            --source_name "${data_source}" \
+            --output "$HOME/public_html/$WEB_BASE_PATH/${data_source}" \
+            --gen $gen \
+            --num_days $MONITOR_NUM_DAYS 
+            # --first_day=20210501 --last_day=20210515
+    done
 done
 
-# Render the webpage
+# Render the summary webpage
 #
-python3 render_summary_page.py $WEBDIR \
-    auxtel_ccs \
-    auxtel_arc \
-    comcam_ccs \
-    comcam_arc \
-    nts_auxtel \
-    nts_comcam 
-    # bot
+python3 render_summary_page.py $WEBDIR "${data_sources[*]}"
